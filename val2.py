6754b966b087381255a4e1883c9e5ec9018b1b6e
jean royannez <jroyannez@gmail.com>
2023-03-12 01:13:30 +0100

    premiere integration streamlit

--- val2.py
+++ val2.py
@@ -1,21 +1,28 @@
 import requests
 import re
+import streamlit as st
+from bs4 import BeautifulSoup
 
-joueur = input('entrer un nom de joueur :')
+
+st.title('FIND YOUR PLAYERS')
+st.write("La data au coeur de votre club de foot professionnel")
+
+
+joueur = st.text_input("Entrez le nom d'un joueur (ou tapez exit pour quitter):" )
+
 
 url = "https://www.transfermarkt.co.uk/ian-raeymaekers/profil/spieler/"+(joueur)
 
-
 navigator = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
-
 html = requests.get(url, headers={'User-Agent': navigator})
-from bs4 import BeautifulSoup
-
 soup = BeautifulSoup(html.text, 'html.parser')
 
-items= soup.find(class_="tm-player-market-value-development__max-value").text
-valval= soup.find(class_='data-header__club-info').text
 
-print(items)
-print(valval)
+
+val=soup.find(class_="tm-player-market-value-development__max-value").text
+club= soup.find(class_='data-header__club-info').text
+val
+club
+
+    
 
